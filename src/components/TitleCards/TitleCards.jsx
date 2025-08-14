4eeda460e8d82ae1c55fe21b195cca4910cbc3fc
DArtangnon <dartangnon101@hotmail.com>
2025-08-13 20:27:28 -0700

    First API update

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './TitleCards.css';
 import cards_data from '../../assets/cards/Cards_data';
 
@@ -6,7 +6,18 @@ import cards_data from '../../assets/cards/Cards_data';
 
 export default function TitleCards({title, category}) {
 
+const [apiData, setApiData] = useState([]);
 const cardsRef = useRef();
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOTNmMjliNjk0NTdlOTk0YTQ1MmJmN2ZmMjJiOWM4ZCIsIm5iZiI6MTc1NTAyOTkwNC42MjEsInN1YiI6IjY4OWJhMTkwYjhhOWQwNjZlNDhlYjZhMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KjS5FtQl9tSj_wYWBSTovr9p2d2dulMzhs7g-sXm4Qk'
+  }
+};
+
+
 
 const handleWheel = (event)=>{
   event.preventDefault;
@@ -14,6 +25,13 @@ const handleWheel = (event)=>{
 }
 
 useEffect(()=>{
+
+fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+  .then(res => res.json())
+  .then(res => setApiData(response.results))
+  .catch(err => console.error(err));
+
+
   cardsRef.current.addEventListener('wheel', handleWheel);
 },[])
 
@@ -21,10 +39,10 @@ useEffect(()=>{
     <div className='title-cards'>
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index)=>{
+        {apiData.map((card, index)=>{
           return <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
+            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
           </div>
         })}
 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import logo from '../../assets/logo.png';
 
 export default function Login() {
+
+const [signState,setSignState] = useState("Sign In")
+
+
   return (
     <div className='login'>
       <img src={logo} className='login-logo' alt="" />
       <div className="login-form">
-        <h1>Sign up</h1>
+        <h1>{signState}</h1>
         <form>
-          <input type="text" placeholder='Your Name' />
+          {signState==="Sign Up"?<input type="text" placeholder='Your Name' />:<></>}
+          
           <input type="email" placeholder='Email' />
           <input type="password" placeholder='Password' />
-          <button>Sign up</button>
+          <button>{signState}</button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
@@ -21,6 +26,12 @@ export default function Login() {
             <p>Need Help?</p>
           </div>
         </form>
+        <div className="form-switch">
+          {signState==="Sign In"?
+          <p>New To Netflix? <span onClick={()=> {setSignState("Sign Up")}}>Sign Up Now</span></p>
+          :<p>Already Have An Account? <span onClick={()=> {setSignState("Sign In")}}>Sign In Now</span></p>}
+          
+        </div>
       </div>
     </div>
   )
