4abddbe2855c0ce9e0338a300cf41fb48ef073c8
DArtangnon <dartangnon101@hotmail.com>
2025-08-18 21:31:56 -0700

    Troubleshooting

---
+++ src/pages/Account.jsx
@@ -0,0 +1,12 @@
+
+export default function Account() {
+  return (
+    <div style={{ padding: '100px 6%', color: '#fff' }}>
+      <h1 style={{ margin: 0 }}>Account</h1>
+      <p style={{ opacity: 0.8 }}>
+        Your saved titles and settings will appear here.
+      </p>
+    </div>
+  );
+}
+
